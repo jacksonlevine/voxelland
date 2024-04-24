547d1cf326891633138769c49aef41a56f508fc9
Jackson Levine <jack@netbug.us>
2024-04-23 17:45:39 -0700

    add comment on test

--- src/tests.rs
+++ src/tests.rs
@@ -4,6 +4,17 @@ use crate::packedvertex::PackedVertex;
 #[test]
 fn test_coord_packing() {
 
+    /*Supposed to be: */
+    /*In (u32, u8) tuple: */
+
+    /*u32*/
+    /*0000 0000 0000 0000 0000 0000 0000 0000  */
+    /*x    y         z    cor  amb  bl   empty */
+
+    /*u8*/
+    /*0000 0000 */
+    /*u    v    */
+
     assert_eq!((0b0000_0000_0000_0000_0000_0000_0000_0000, 0b0000_0000), PackedVertex::pack(0, 0, 0, 0, 0, 0, 0, 0));
     assert_eq!((0b0001_0000_0001_0001_0000_0000_0000_0000, 0b0000_0000), PackedVertex::pack(1, 1, 1, 0, 0, 0, 0, 0));
     assert_eq!((0b0010_1000_0000_0010_0010_1110_0001_0000, 0b0000_0000), PackedVertex::pack(2, 128, 2, 2, 14, 1, 0, 0));

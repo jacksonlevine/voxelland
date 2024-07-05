f68ccd312845e5fac7cece251a1a23a0f1355f1d
Jackson Levine <jack@netbug.us>
2024-07-05 15:02:19 -0700

    Update recipes.rs

--- lib/src/recipes.rs
+++ lib/src/recipes.rs
@@ -4,7 +4,8 @@ pub type Recipe = (Vec<(u32, u32)>, (u32, u32));
 
 
 
-pub static RECIPES: Lazy<[Recipe; 2]> = Lazy::new(|| [
+pub static RECIPES: Lazy<[Recipe; 3]> = Lazy::new(|| [
     (vec![(6, 1)], (10, 4)),
     (vec![(10, 4)], (19, 2)),
+    (vec![(10, 2)], (20, 10)),
 ]);

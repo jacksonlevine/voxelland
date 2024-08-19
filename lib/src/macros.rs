d62dfb646d7b575de686ef369b0ce7b24db6763c
CumMan7 <puppercumman@gmail.com>
2024-08-18 22:44:49 -0400

    cfg macros for conditional compilation, path conditionally compiles for steam vs non-steam builds

--- lib/src/macros.rs
+++ lib/src/macros.rs
@@ -1,6 +1,71 @@
 #[macro_export]
-macro_rules! path {
-    ($path:literal) => {
-        concat!(env!("CARGO_MANIFEST_DIR"), "/../", $path)
+macro_rules! cfg_steam { 
+    ($($item:item)*) => {
+        $(
+            #[cfg(feature = "steam")]
+            $item
+        )*
     }
+}
+
+#[macro_export]
+macro_rules! cfg_no_steam {
+    ($($item:item)*) => {
+        $(
+            #[cfg(not(feature = "steam"))]
+            $item
+        )*
+    }
+}
+
+#[macro_export]
+macro_rules! cfg_audio {
+    ($($item:item)*) => {
+        $(
+            #[cfg(feature = "audio")]
+            $item
+        )*
+    } 
+}
+
+#[macro_export]
+macro_rules! cfg_no_audio {
+    ($($item:item)*) => {
+        $(
+            #[cfg(not(feature = "audio"))]
+            $item
+        )*
+    } 
+}
+
+#[macro_export]
+macro_rules! cfg_no_glfw {
+    ($($item:item)*) => {
+        $(
+            #[cfg(not(feature = "glfw"))]
+            $item
+        )*
+    }
+}
+
+#[macro_export]
+macro_rules! cfg_glfw {
+    ($($item:item)*) => {
+        $(
+            #[cfg(feature = "glfw")]
+            $item
+        )*
+    }
+}
+
+#[macro_export]
+macro_rules! path {
+    ($path:literal) => {{
+        #[cfg(not(feature = "steam"))] {
+            concat!(env!("CARGO_MANIFEST_DIR"), "/../", $path)
+        }
+        #[cfg(feature = "steam")] {
+            $path
+        }
+    }}
 }
